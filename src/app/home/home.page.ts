78c5d1aaef82d016f7d5b29d7aa4d8ebc9b05371
Magda <magdacorrea2@gmail.com>
2025-11-25 00:21:45 -0500

    Se agrega la funcionalidad para filtrar por id en la vista

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,27 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { RouterModule } from '@angular/router';
 import { addIcons } from 'ionicons';
 import { add, trash } from 'ionicons/icons';
 import { GetAllTaskUseCase } from '../../domain/usecases/get-all-tasks-usecase';
 import { Task } from '../../domain/models/task/task';
-
 import { DeleteTaskUseCase } from '../../domain/usecases/delete-task-usecase';
+import { GetTaskByIdUseCase } from '../../domain/usecases/get-task-by-id-use-case';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [CommonModule, IonicModule, RouterModule],
+  imports: [CommonModule, IonicModule, RouterModule, FormsModule],
 })
 export class HomePage {
   tasks: Task[] = [];
 
   constructor(
     private getAllTaskUseCase: GetAllTaskUseCase,
-    private deleteTaskUseCase: DeleteTaskUseCase
+    private deleteTaskUseCase: DeleteTaskUseCase,
+    private getTaskByIdUseCase: GetTaskByIdUseCase
   ) {
     addIcons({ add, trash });
   }
@@ -32,6 +34,17 @@ export class HomePage {
 
   async loadTasks() {
     this.tasks = await this.getAllTaskUseCase.run();
+  }
+
+  async handleSearch(event: any) {
+    const query = event.target.value;
+
+    if (query && query.trim() !== '') {
+      const task = await this.getTaskByIdUseCase.run(query);
+      this.tasks = task ? [task] : [];
+    } else {
+      await this.loadTasks();
+    }
   }
 
   async deleteTask(id: string) {
