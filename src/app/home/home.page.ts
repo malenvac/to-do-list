4c4eeb5ac6904e9dfdfb7d1ff644d52b9cc9b85c
Magda <magdacorrea2@gmail.com>
2025-11-25 21:04:15 -0500

    se añaden colores para estilos globales, y diseño para home

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,16 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { RouterModule } from '@angular/router';
 import { addIcons } from 'ionicons';
-import { add, trash, create } from 'ionicons/icons';
+import { add, trash, create, gridOutline, ellipsisHorizontal } from 'ionicons/icons';
 import { Task } from '../../domain/models/task/task';
 import { GetAllTaskUseCase } from 'src/domain/usecases/task/get-all-tasks-usecase';
 import { DeleteTaskUseCase } from 'src/domain/usecases/task/delete-task-usecase';
 import { GetTaskByIdUseCase } from 'src/domain/usecases/task/get-task-by-id-use-case';
 import { UpdateTaskUseCase } from 'src/domain/usecases/task/update-task-use-case';
-
 
 @Component({
   selector: 'app-home',
@@ -21,6 +20,7 @@ import { UpdateTaskUseCase } from 'src/domain/usecases/task/update-task-use-case
 })
 export class HomePage {
   tasks: Task[] = [];
+  today = '';
 
   constructor(
     private getAllTaskUseCase: GetAllTaskUseCase,
@@ -28,7 +28,15 @@ export class HomePage {
     private getTaskByIdUseCase: GetTaskByIdUseCase,
     private updateTaskUseCase: UpdateTaskUseCase
   ) {
-    addIcons({ add, trash, create });
+    addIcons({ add, trash, create, gridOutline, ellipsisHorizontal });
+
+    const now = new Date();
+    const formatter = new Intl.DateTimeFormat('es-ES', {
+      day: 'numeric',
+      month: 'long',
+      year: 'numeric',
+    });
+    this.today = formatter.format(now); // ej: "25 de noviembre de 2025"
   }
 
   async ionViewWillEnter() {
@@ -41,7 +49,6 @@ export class HomePage {
 
   async handleSearch(event: any) {
     const query = event.target.value;
-
     if (query && query.trim() !== '') {
       const task = await this.getTaskByIdUseCase.run(query);
       this.tasks = task ? [task] : [];
